134422415a444cd0d306e91a1e9dbe3f4fb79023
andreklaroskleao <andre.klar.leao@gmail.com>
2026-03-19 00:03:45 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -15,4 +15,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export { signInWithPopup, signOut }; // Exportando para o app.js usar
+export { signInWithPopup, signOut };
